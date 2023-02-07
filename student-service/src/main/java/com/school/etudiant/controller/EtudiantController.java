b3d67019a6084aef1f7593008bbb0e6c4dd64da8
Amine Aboutaj <90484039+MiBoutaj@users.noreply.github.com>
2023-02-07 21:47:34 +0100

    Start docker

--- student-service/src/main/java/com/school/etudiant/controller/EtudiantController.java
+++ student-service/src/main/java/com/school/etudiant/controller/EtudiantController.java
@@ -6,6 +6,7 @@ import com.school.etudiant.model.Etudiant;
 import com.school.etudiant.service.EtudiantServcieImpl;
 import com.school.etudiant.dto.EtudiantRequeste;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,26 +20,31 @@ public class EtudiantController {
     EtudiantServcieImpl etudiantServcie;
 
     @PostMapping("/add")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public EtudiantResponse addEtudiant(@RequestBody EtudiantRequeste etudiant){
         return etudiantServcie.addEtudiant(etudiant);
     }
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public Etudiant updateEtudiant(@PathVariable Long id , @RequestBody Etudiant e){
       return etudiantServcie.editEtudiant(id,e);
 
     }
 
     @GetMapping("/etudiant/{id}")
+    @PreAuthorize("hasAnyAuthority('USER')")
     public Etudiant getEtudiant(@PathVariable Long id){
     return etudiantServcie.findById(id);
     }
     @GetMapping("/etudiants")
+    @PreAuthorize("hasAnyAuthority('USER')")
     public List<Etudiant> geAlltEtudiant(){
         return etudiantServcie.findAll();
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public void deleteEtudiant(@RequestParam Long id){
         etudiantServcie.deleteById(id);
     }
